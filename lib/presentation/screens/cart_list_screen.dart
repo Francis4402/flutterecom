ced602cc7af99849fa4b7aa9bb8865295c6b6f73
Francis <francism4402@gmail.com>
2024-10-23 22:59:19 +0600

    cart list update

--- lib/presentation/screens/cart_list_screen.dart
+++ lib/presentation/screens/cart_list_screen.dart
@@ -1,10 +1,8 @@
 import 'package:ecommerceflutter/presentation/state_holders/main_bottom_nav_bar_controller.dart';
-import 'package:ecommerceflutter/presentation/utility/app_colors.dart';
-import 'package:ecommerceflutter/presentation/utility/asset_path.dart';
+import 'package:ecommerceflutter/presentation/widgets/card_product_item.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
-import 'package:item_count_number_button/item_count_number_button.dart';
 
 class CartListScreen extends StatefulWidget {
   const CartListScreen({super.key});
@@ -31,91 +29,9 @@ class _CartListScreenState extends State<CartListScreen> {
               },
               icon: const Icon(Icons.arrow_back_ios_sharp)),
         ),
-        body: Column(
+        body: const Column(
           children: [
-            Card(
-              elevation: 4,
-              surfaceTintColor: Colors.white,
-              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SvgPicture.asset(
-                      AssetsPath.productDummyImgSvg,
-                      width: 100,
-                    ),
-                  ),
-                  Expanded(
-                      child: Column(
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Expanded(
-                              child: Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Fake Shoe',
-                                  maxLines: 1,
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.black,
-                                    fontWeight: FontWeight.w600,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ),
-                                Wrap(
-                                  spacing: 16,
-                                  children: [
-                                    Text(
-                                      'Color: Red',
-                                      style: TextStyle(color: Colors.black54),
-                                    ),
-                                    Text(
-                                      'Size: 9',
-                                      style: TextStyle(color: Colors.black54),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
-                          )),
-                          IconButton(
-                              onPressed: () {},
-                              icon: const Icon(Icons.delete_outline_sharp))
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            '\$100',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 16,
-                                color: AppColors.primaryColor),
-                          ),
-                          ItemCount(
-                              initialValue: 1,
-                              minValue: 1,
-                              maxValue: 20,
-                              color: AppColors.primaryColor,
-                              onChanged: (value) {
-
-                                setState(() {});
-                              },
-                              decimalPlaces: 0)
-                        ],
-                      ),
-                    ],
-                  ))
-                ],
-              ),
-            )
+             CardProductItem()
           ],
         ),
       ),
